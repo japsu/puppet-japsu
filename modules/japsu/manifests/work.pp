1ca4f15b9e9c90fc72577f04a05c7420ef8088e6
Santtu Pajukanta <santtu@pajukanta.fi>
2013-05-06 13:49:09 +0300

    oops

---
+++ modules/japsu/manifests/work.pp
@@ -0,0 +1,42 @@
+class japsu::work {
+  include japsu::basics
+  include japsu::work::network
+  include japsu::work::samba
+}
+
+class japsu::work::network($last_octet='3') {
+  augeas{ "host_only_networking":
+    context => "/files/etc/network/interfaces",
+    changes => [
+      "set auto[child::1 = 'eth1']/1 eth1",
+      "set iface[. = 'eth1'] eth1",
+      "set iface[. = 'eth1']/family inet",
+      "set iface[. = 'eth1']/method static",
+      "set iface[. = 'eth1']/address 192.168.56.$last_octet",
+      "set iface[. = 'eth1']/netmask 255.255.255.0",
+      "set iface[. = 'eth1']/network 192.168.56.0",
+    ];
+}
+
+class japsu::work::samba {
+  class { 'samba::server':
+    workgroup            => 'WORKGROUP',
+    server_string        => 'Virtual Machine for Work',
+    netbios_name         => "$hostname",
+    interfaces           => [ 'lo', 'eth1' ],
+    hosts_allow          => [ '127.', '192.168.56.' ],
+    local_master         => 'no',
+    map_to_guest         => 'Bad User',
+    os_level             => '50',
+    preferred_master     => 'no',
+    shares => {
+        'work' => [
+            'comment = Work',
+            'writable = yes',
+            'guest ok = no',
+            'valid users = japsu',
+        ],
+    },
+    selinux_enable_home_dirs => true,
+    }
+}
