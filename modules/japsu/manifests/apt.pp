8821fa0cad2ee8908bec4c89e9c71200f1a88e92
Santtu Pajukanta <santtu@pajukanta.fi>
2013-05-07 09:24:20 +0300

    jumalaton tunkkaus apt-filen vuoksi

--- modules/japsu/manifests/apt.pp
+++ modules/japsu/manifests/apt.pp
@@ -4,6 +4,13 @@ class japsu::apt {
       ensure => present;
   }
 
+  exec {
+    'apt_file_initial_update':
+      require => Package['apt-file'],
+      onlyif => "/bin/bash -c '[ -n \"/var/cache/apt/apt-file/*\" ]'",
+      command => '/usr/bin/apt-file update';
+  }
+
   file {
     '/etc/apt/apt.conf.d/00jumalauta':
       ensure => present,
