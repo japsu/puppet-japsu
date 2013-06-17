a1714cfca0abc27091cece6afa4ee5a40103ec88
Santtu Pajukanta <santtu.pajukanta@leonidasoy.fi>
2013-06-17 10:14:50 +0300

    disable some modules that are useless in a virtualized environment

--- modules/virtualbox/manifests/guest.pp
+++ modules/virtualbox/manifests/guest.pp
@@ -8,4 +8,21 @@ class virtualbox::guest {
     ]:
       ensure => present;
   }
+
+  exec {
+    'update-initramfs':
+      command => '/usr/sbin/update-initramfs -u -k all',
+      refreshonly => true;
+  }
+
+  file {
+    # disable modules that are useless in a virtual environment
+    '/etc/modprobe.d/virtualbox-blacklist.conf':
+      ensure => present,
+      owner => 'root',
+      group => 'root',
+      mode => 0644,
+      source => 'puppet:///modules/virtualbox/module-blacklist.conf',
+      notify => Exec['update-initramfs'];
+  }
 }
