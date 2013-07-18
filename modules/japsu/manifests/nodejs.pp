df88c935a061e8c95b3d6e6e87c3b8ab5dde1915
Santtu Pajukanta <santtu@pajukanta.fi>
2013-07-19 01:29:50 +0300

    fix commit

---
+++ modules/japsu/manifests/nodejs.pp
@@ -0,0 +1,13 @@
+class japsu::nodejs {
+  include ::nvm
+  
+  nvm::user_nvm {
+    'japsu':
+      autoload => true;
+  }
+
+  nvm::user_node {
+    'japsu/v0.10.13':
+      set_as_default => true;
+  }
+}
