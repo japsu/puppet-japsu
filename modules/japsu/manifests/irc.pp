bee256de61351569aaa441aa2e5b905b91111438
Santtu Pajukanta <santtu@pajukanta.fi>
2013-07-18 21:37:12 +0300

    service for oidentd

--- modules/japsu/manifests/irc.pp
+++ modules/japsu/manifests/irc.pp
@@ -10,7 +10,16 @@ class japsu::irc {
   file {
     '/etc/oidentd.conf':
       require => Package['oidentd'],
+      notify => Service['oidentd'],
       ensure => present,
       source => 'puppet:///modules/japsu/oidentd.conf';
+  }
+
+  service {
+    'oidentd':
+      ensure => running,
+      enable => true,
+      hasrestart => true,
+      hasstatus => false;
   }
 }
