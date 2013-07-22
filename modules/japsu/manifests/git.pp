fbbf4c2469a25ab16583a27d67ab7e88be6212cd
Santtu Pajukanta <santtu.pajukanta@leonidasoy.fi>
2013-07-22 16:40:07 +0300

    debian stable git does not support push.default=simple

--- modules/japsu/manifests/git.pp
+++ modules/japsu/manifests/git.pp
@@ -6,6 +6,11 @@ class japsu::git($email_address='santtu@pajukanta.fi') {
       ensure => present;
   }
 
+  $git_push_default_behaviour = $::operatingsystem ? {
+    'Ubuntu' => 'simple',
+    'Debian' => 'tracking'
+  }
+
   file {
     '/home/japsu/.gitconfig':
       ensure => present,
