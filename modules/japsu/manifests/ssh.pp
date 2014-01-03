624d11184fce5f5e97603f47c3bfca49c20f82aa
Santtu Pajukanta <santtu@pajukanta.fi>
2014-01-03 23:44:28 +0200

    new ssh key

--- modules/japsu/manifests/ssh.pp
+++ modules/japsu/manifests/ssh.pp
@@ -31,6 +31,12 @@ class japsu::ssh::keys::personal {
     'japsu@navitas-20130507':
       user => 'japsu',
       ensure => absent;
+    'japsu@ishtar-20130507':
+      ensure => absent,
+      user => 'japsu';
+    'japsu@nemesis-20130507':
+      ensure => absent,
+      user => 'japsu';
     'japsu@navitas-20130719':
       ensure => present,
       key => 'AAAAB3NzaC1yc2EAAAABJQAAAQBm2r27t6Z/M5vfiXhjlGBkv94QilQRlwexlipgt+q1CfaKKlBJAS85X5/OJj4ATT9ijh+9Y+p4AYjO3OnwvSKLD6UjQwchEe28RJ1X3o85wJ12TpHXhWWl2rCF0uXrYTJom1fcUyC9eqy55l7M6UT3p7jeYKeFnhyRw4JnN3jAMX839504k/u304Rz7Hwl0aSJ6y9mOdvYzqS52OR0YQBJI4c8CPkdaywAYlCWSzB//M2JSBtm/QKwfKZwmqq8JNc19+Wyzr2sRWG876njbQia2WWlQ42/NbVmsCcuuPuVH3kwcdaIgnQDaqpIqLhs9ydSYYo7Fg83oF9PzHwsM6ct',
@@ -41,17 +47,14 @@ class japsu::ssh::keys::personal {
       key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDUnl7Av8IOOyzOliNQ23+py3OY6jQCQdjTFDTWVCoF7AeX5UpRFwvklP8p7E70ZFwB1oSSfxjJhsHaUQrlhVr1YXGATYdiUZfeI7jqKWLee2olc60X9PJ5t4vYUBGk42rwKed1alaQ7pN5CdGMoy5n4nqOGIXTkSr7P7SSVu5H5/AqwTeeFP3SXeWRsFJqMRf8eArlKjQmC1nY4sRcAAPtpu/Zdhgm/AqGliTjiBLDIrvf1NVOaPbgVZcXwvRHo9V7WbJufl4qZaTYhDrWYfCM3eS1jtKJf5oatD0O1xflnejnuxINbNPE+POD1Co41yh1E9i5dYf5LsXtjUznNFfV',
       type => 'ssh-rsa',
       user => 'japsu';
-    'japsu@ishtar-20130507':
-      ensure => present,
-      key => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAhRRYLAvEVLdkMoLTEhLYvEX4mQOM7SXyF5Efil/0iK+fqxLUudHPC3au+jGzwTvfAVY4pfLdgZtyj9z2l6Q0sTcycAYiZnOC6enCF/aeHdDbe1lfgRZ6ZWPu1hnmFsMJwPsWSERix/x0RU5hJdvKGSFGh1n6KWBLd9Wicr/ab3EtpsIDyQrOK5tz7wMUrHdpsw2N19/RYvWlVg+WSA8Oc4RvPyVg+R4Q80DgwgF9L5LgasYGDDjOUHt9BwzMCwK68NH7WEF6A2OT7Cz+0gcoaVDKQa8dFaPZBL8ZuVOsgKzI5bfk29iTLUvkB71x4fTMplNGhIA75KLAKd+frkxzbw==',
-      type => 'ssh-rsa',
-      user => 'japsu';
-    'japsu@nemesis-20130507':
-      ensure => absent,
-      user => 'japsu';
     'japsu@proteus-20131119':
       ensure => present,
       key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDLrbcrhiE2tYJQMhgkeTn2OZ37wxkAE36w2y9508L4AtbnihS5Thf+FzJri/cMfvJTBX5cG+ji2jTrakyQ9NLDwq2KeH1EaRmyBXWrQmrOXrcV7HA5yIsddBdw+e3nyglv/LqbhRpQ2TBC/SprM2zkhW2JyYZzDVOYstIavg/qqa2IeyjUZ0Rcxe85r+PaQwumiQ4sLGnsjK+Ktzq1QjjW2O5gH2NhsYR2OY1MDAMFkWS6Ga6ZzWu3E5f4pyk9HLiKJp8Ozv1Hca0x+DzTLB+TsGkg01K2br3932/V9US1abfduIExyhRVw77WV42Z9whjCPj6aSDHU04xaRQSXNwH',
+      type => 'ssh-rsa',
+      user => 'japsu';
+    'japsu@ishtar-20140103':
+      ensure => present,
+      key => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAqCL13GZ/DVcR5F+fMMZz6aj8KAxdb6UB2FF/iCX0l5lYK/9lYtHCyEDNpwh/BjfbXocPB5z6PVD4FCJ8Aflgk/JrIiAXmqCOdpMlNihHO8oxvWWzGwcZDozgI+8U0umSfp1N+7RWHcyD+j65Fg7ac9AczHzGhHkCuVgatdgb5U39TMclMt1K6ndxEA/gzx7AwramKm6i/4HfM05kA2LazwTL1s7bAeu+z+vX6eKGEv10bbDaRcuU5BeASkB69eM37cd4ZynbH+14giZHPRb1x3rpmdu5fCMdnyJrDFZHmjyCvLelLgYcSPzSF5W1B302VZTLevOTyLNeECt+KdH3xw==',
       type => 'ssh-rsa',
       user => 'japsu';
   }
