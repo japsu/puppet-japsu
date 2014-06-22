f342e808047ca6cedfb3a1804aff2731fe1c12df
Santtu Pajukanta <santtu@pajukanta.fi>
2014-06-22 15:44:59 +0300

    nodejs: v0.10.29

--- modules/japsu/manifests/nodejs.pp
+++ modules/japsu/manifests/nodejs.pp
@@ -1,13 +1,13 @@
 class japsu::nodejs {
   include ::nvm
-  
+
   nvm::user_nvm {
     'japsu':
       autoload => true;
   }
 
   nvm::user_node {
-    'japsu/v0.10.24':
+    'japsu/v0.10.29':
       set_as_default => true;
   }
 }
