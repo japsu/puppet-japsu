a9c8948d35320e2538b2cabaa462754737c5a6b9
Santtu Pajukanta <santtu@pajukanta.fi>
2014-10-30 22:59:46 +0200

    pg_hba

--- modules/postgresql/manifests/init.pp
+++ modules/postgresql/manifests/init.pp
@@ -1,9 +1,20 @@
-class postgresql {
+class postgresql(
+    $postgresql_version = '9.3'
+) {
     package {
         [
             'postgresql',
             'libpq-dev'
         ]:
             ensure => present;
+    }
+
+    file {
+        "/etc/postgresql/$postgresql_version/main/pg_hba.conf":
+          require => Package['postgresql'],
+          ensure => present,
+          source => ["puppet:///modules/postgresql/pg_hba.conf@${::hostname}",
+                     'puppet:///modules/postgresql/pg_hba.conf'],
+          notify => Service['postgresql'];            
     }
 }
